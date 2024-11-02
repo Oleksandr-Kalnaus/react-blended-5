37e10cfe494071206a372fd9ab5edaeec4a467f4
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-11-02 13:00:20 +0200

    addForm

--- src/service/exchangeAPI.js
+++ src/service/exchangeAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://api.apilayer.com/exchangerates_data/',
-  headers: { apikey: '' },
+  headers: { apikey: 'mdnq3rPIdQhDDYvUFApjCzm4beHfpBCL' },
 });
 
 export const exchangeCurrency = async credentials => {
