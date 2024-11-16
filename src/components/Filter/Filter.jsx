18c87a3029aaf9f72c7077541f916f581ebcd40b
Andriy Dzhuryn <165391426+AndriyDzhuryn@users.noreply.github.com>
2024-11-16 12:36:06 +0200

    final

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Filter.module.css';
+import { selectFilter } from 'reduxState/selectors';
+import { setFilter } from 'reduxState/filterSlice';
 export const Filter = () => {
+  const value = useSelector(selectFilter);
+  const dispatch = useDispatch();
+
+  const handleChange = event => {
+    dispatch(setFilter(event.target.value));
+  };
+
   return (
     <input
+      value={value}
+      onChange={handleChange}
       placeholder="What currency are you looking for?🧐"
       className={styles.input}
     />
--- src/reduxState/selectors.js
+++ src/reduxState/selectors.js
@@ -6,11 +6,17 @@ export const selectLoading = state => state.currency.isLoading;
 export const selectError = state => state.currency.isError;
 export const selectRates = state => state.currency.rates;
 
+export const selectFilter = state => state.filter.value;
+
 export const selectFilteredRates = createSelector(
-  [selectRates, selectBaseCurrency],
-  (rates, baseCurrency) => {
+  [selectRates, selectBaseCurrency, selectFilter],
+  (rates, baseCurrency, filter) => {
     return rates
-      .filter(([key]) => key !== baseCurrency)
+      .filter(
+        ([key]) =>
+          key !== baseCurrency &&
+          key.toLowerCase().includes(filter.toLowerCase()),
+      )
       .map(([key, value]) => ({ key, value: (1 / value).toFixed(2) }));
   },
 );
